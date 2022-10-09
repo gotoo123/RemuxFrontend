e8d08de71ab6ca177d0a1b569fd999c44d96fa17
“gotoo123” <1219727892@qq.com>
2022-10-09 20:02:47 +0800

    chore: 引用组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Carousel from "./components/carousel";
+import Header from "./components/header"
 const App = () => {
-  return <div>hello</div>
+  return (
+    <>
+      <Header/>
+      <Carousel/>
+    </>
+  );
 };
 
 export default App;
