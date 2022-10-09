96415207b4d1ae5075270e205afbdd834f9d702f
“gotoo123” <1219727892@qq.com>
2022-10-09 20:00:38 +0800

    feat: add carousel

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import './index.less';
+
+interface CarouselProps {
+
+}
+
+const Carousel = (props: CarouselProps) => {
+  const [a, setA] = useState(false);
+  const handleClick = () => {
+     setA(true);
+  }
+
+  return (
+    <div className="gt-carousel">
+        <div className="gt-carousel-shell">
+          <input type="radio" name="music" id="item-1" />
+          <input type="radio" name="music" id="item-2" />
+          <input type="radio" name="music" id="item-3"  />
+          <div className="gt-carousel-cards">
+            <label className="gt-carousel-card" htmlFor="item-1" id="song-1">
+              <div className="gt-carousel-content">content1</div>
+            </label>
+            <label className="gt-carousel-card" htmlFor="item-2" id="song-2">
+              <div className="gt-carousel-content">content2</div>
+            </label>
+            <label className="gt-carousel-card" htmlFor="item-3" id="song-3" >
+              <div className="gt-carousel-content" onClick={handleClick}>
+                {a ? 'content3' : 'Click me'}
+              </div>
+            </label>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Carousel;
