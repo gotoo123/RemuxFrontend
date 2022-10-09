b35c5690e67efe6eb201e9e18f03554fbd7fd292
“gotoo123” <1219727892@qq.com>
2022-10-09 20:00:13 +0800

    feat: add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.less';
+
+const Header = () => {
+  return (
+    <div className="header-content">
+      <div className="header-logo">Remux</div>
+      <div className="header-nav">
+        <div className="nav-item">登录</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
